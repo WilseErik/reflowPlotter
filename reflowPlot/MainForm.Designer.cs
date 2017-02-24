c3c910488432372de8121b43b0778033e72e77ef
WilseErik <erik.wilstermann@hotmail.com>
2017-02-24 17:50:33 +0100

    Initial commit.

---
+++ reflowPlot/MainForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace reflowPlot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.temperatureChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.errorChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.servoChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.heaterChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.comNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sysTick = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.temperatureChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.servoChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heaterChart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.comNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // temperatureChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.temperatureChart.ChartAreas.Add(chartArea1);
+            this.temperatureChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.temperatureChart.Legends.Add(legend1);
+            this.temperatureChart.Location = new System.Drawing.Point(4, 4);
+            this.temperatureChart.Margin = new System.Windows.Forms.Padding(4);
+            this.temperatureChart.Name = "temperatureChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperature";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Target temp";
+            this.temperatureChart.Series.Add(series1);
+            this.temperatureChart.Series.Add(series2);
+            this.temperatureChart.Size = new System.Drawing.Size(582, 275);
+            this.temperatureChart.TabIndex = 0;
+            this.temperatureChart.Text = "chart1";
+            // 
+            // errorChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.errorChart.ChartAreas.Add(chartArea2);
+            this.errorChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.errorChart.Legends.Add(legend2);
+            this.errorChart.Location = new System.Drawing.Point(594, 4);
+            this.errorChart.Margin = new System.Windows.Forms.Padding(4);
+            this.errorChart.Name = "errorChart";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Error";
+            this.errorChart.Series.Add(series3);
+            this.errorChart.Size = new System.Drawing.Size(583, 275);
+            this.errorChart.TabIndex = 1;
+            this.errorChart.Text = "chart2";
+            // 
+            // servoChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.servoChart.ChartAreas.Add(chartArea3);
+            this.servoChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Name = "Legend1";
+            this.servoChart.Legends.Add(legend3);
+            this.servoChart.Location = new System.Drawing.Point(4, 287);
+            this.servoChart.Margin = new System.Windows.Forms.Padding(4);
+            this.servoChart.Name = "servoChart";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Servo";
+            this.servoChart.Series.Add(series4);
+            this.servoChart.Size = new System.Drawing.Size(582, 276);
+            this.servoChart.TabIndex = 3;
+            this.servoChart.Text = "chart3";
+            // 
+            // heaterChart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.heaterChart.ChartAreas.Add(chartArea4);
+            this.heaterChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this.heaterChart.Legends.Add(legend4);
+            this.heaterChart.Location = new System.Drawing.Point(594, 287);
+            this.heaterChart.Margin = new System.Windows.Forms.Padding(4);
+            this.heaterChart.Name = "heaterChart";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Heater";
+            this.heaterChart.Series.Add(series5);
+            this.heaterChart.Size = new System.Drawing.Size(583, 276);
+            this.heaterChart.TabIndex = 2;
+            this.heaterChart.Text = "chart4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(612, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PID output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(612, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Servo position";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1387, 588);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tableLayoutPanel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 18);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1181, 567);
+            this.panel2.TabIndex = 11;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.temperatureChart, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.servoChart, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.errorChart, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.heaterChart, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1181, 567);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1184, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 567);
+            this.panel1.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.disconnectButton);
+            this.groupBox2.Controls.Add(this.connectButton);
+            this.groupBox2.Controls.Add(this.comNumericUpDown);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 567);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Connection";
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(6, 116);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(120, 23);
+            this.disconnectButton.TabIndex = 3;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(6, 87);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(120, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // comNumericUpDown
+            // 
+            this.comNumericUpDown.Location = new System.Drawing.Point(6, 59);
+            this.comNumericUpDown.Name = "comNumericUpDown";
+            this.comNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.comNumericUpDown.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM Port number";
+            // 
+            // sysTick
+            // 
+            this.sysTick.Enabled = true;
+            this.sysTick.Interval = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 588);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Reflow oven data visualisation v0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.temperatureChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.servoChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heaterChart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.comNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart temperatureChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart errorChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart servoChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart heaterChart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.NumericUpDown comNumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer sysTick;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ reflowPlot/MainForm.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Globalization;
+
+namespace reflowPlot
+{
+    public partial class MainForm : Form
+    {
+        private List<double> temperatureXList = new List<double>();
+        private List<double> temperatureYList = new List<double>();
+        private List<double> errorXList = new List<double>();
+        private List<double> errorYList = new List<double>();
+        private List<double> servoXList = new List<double>();
+        private List<double> servoYList = new List<double>();
+        private List<double> heaterXList = new List<double>();
+        private List<double> heaterYList = new List<double>();
+        string tempSeriesName = "Temperature";
+        string errorSeriesName = "Error";
+        string servoSeriesName = "Servo";
+        string heaterSeriesName = "Heater";
+        BackgroundWorker comLineReader;
+
+        SerialPort serialPort;
+        string serialPortLine = "";
+        bool serialConnected = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            connectButton.Enabled = true;
+            disconnectButton.Enabled = false;
+        }
+
+        private void AddTempValue(double time, double temp)
+        {
+            temperatureXList.Add(time);
+            temperatureYList.Add(temp);
+
+            if (temperatureChart.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate { UpdateTempChart(); });
+            }
+        }
+
+        private void UpdateTempChart()
+        {
+            temperatureChart.Series[tempSeriesName].Points.Clear();
+
+            for (int i = 0; i != temperatureXList.Count; ++i)
+            {
+                temperatureChart.Series[tempSeriesName].Points.AddXY(temperatureXList[i], temperatureYList[i]);
+            }
+        }
+
+        private void AddErrorValue(double time, double val)
+        {
+            errorXList.Add(time);
+            errorYList.Add(val);
+
+            if (errorChart.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate { UpdateErrorChart(); });
+            }
+        }
+
+        private void UpdateErrorChart()
+        {
+            errorChart.Series[errorSeriesName].Points.Clear();
+
+            for (int i = 0; i != errorXList.Count; ++i)
+            {
+                errorChart.Series[errorSeriesName].Points.AddXY(errorXList[i], errorYList[i]);
+            }
+        }
+
+        private void AddServoValue(double time, double val)
+        {
+            servoXList.Add(time);
+            servoYList.Add(val);
+
+            if (servoChart.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate { UpdateServoChart(); });
+            }
+        }
+
+        private void UpdateServoChart()
+        {
+            servoChart.Series[servoSeriesName].Points.Clear();
+
+            for (int i = 0; i != servoXList.Count; ++i)
+            {
+                servoChart.Series[servoSeriesName].Points.AddXY(servoXList[i], servoYList[i]);
+            }
+        }
+
+        private void AddHeaterValue(double time, double val)
+        {
+            heaterXList.Add(time);
+            heaterYList.Add(val);
+
+            if (heaterChart.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate { UpdateHeaterChart(); });
+            }
+        }
+
+        private void UpdateHeaterChart()
+        {
+            heaterChart.Series[heaterSeriesName].Points.Clear();
+
+            for (int i = 0; i != heaterXList.Count; ++i)
+            {
+                heaterChart.Series[heaterSeriesName].Points.AddXY(heaterXList[i], heaterYList[i]);
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort = new SerialPort();
+                serialPort.BaudRate = 9600;
+                serialPort.DataBits = 8;
+                serialPort.Handshake = Handshake.None;
+                serialPort.Parity = Parity.None;
+                serialPort.PortName = "COM" + comNumericUpDown.Value;
+                serialPort.StopBits = StopBits.One;
+                serialPort.NewLine = "\n";
+                serialPort.Open();
+
+                serialConnected = true;
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+
+                comLineReader = new BackgroundWorker();
+                comLineReader.DoWork += new DoWorkEventHandler(DoComPortReadWork);
+                comLineReader.RunWorkerCompleted += new RunWorkerCompletedEventHandler(ComPortReadComplete);
+                comLineReader.RunWorkerAsync();
+            }
+            catch (Exception)
+            {
+                ;
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            disconnectComPort();
+        }
+
+        private void disconnectComPort()
+        {
+            try
+            {
+                serialPort.Close();
+            }
+            catch (Exception) { }
+
+            serialConnected = false;
+
+            connectButton.Enabled = true;
+            disconnectButton.Enabled = false;
+        }
+
+        private void DoComPortReadWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                this.serialPortLine = this.serialPort.ReadLine();
+            }
+            catch (InvalidOperationException)
+            {
+                disconnectComPort();
+            }
+            catch (TimeoutException)
+            {
+                ;
+            }
+        }
+
+        private void ComPortReadComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string[] values = this.serialPortLine.Split(';');
+
+            // The values are formated as:
+            //temperature;time;heater duty;servo pos;pid
+            double temp = 0;
+            double time = 0;
+            double heater = 0;
+            double servo = 0;
+            double targetTemp = 0;
+
+            if (5 == values.Length)
+            {
+                bool parseOk = false;
+
+                try
+                {
+                    temp = Double.Parse(values[0], CultureInfo.InvariantCulture);
+                    time = Double.Parse(values[1], CultureInfo.InvariantCulture);
+                    heater = Double.Parse(values[2], CultureInfo.InvariantCulture);
+                    servo = Double.Parse(values[3], CultureInfo.InvariantCulture);
+                    targetTemp = Double.Parse(values[4], CultureInfo.InvariantCulture);
+
+                    parseOk = true;
+                }
+                catch (Exception)
+                {
+                    parseOk = false;
+                }
+
+                if (parseOk)
+                {
+                    AddTempValue(time, temp);
+                    AddHeaterValue(time, heater);
+                    AddServoValue(time, servo);
+                    AddErrorValue(time, temp - targetTemp);
+                }
+            }
+
+            comLineReader.RunWorkerAsync();
+        }
+    }
+}
